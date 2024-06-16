a2cb3159a9fb9dcce7458286f9c1273e6abd62ae
GOURAV0188 <guptagourav0306@gmail.com>
2024-06-16 14:39:29 +0530

    add files

---
+++ index2.py
@@ -0,0 +1,293 @@
+# # print("jii")
+
+# # print("hello wordl")
+
+
+
+# # # for  multiple  lines 
+# # # 1. to  insert  a  multiple  line  use  triple  quation mark 
+# # # 2.   to insert  a  \n (back slash)
+
+# # print("""
+      
+# #        hello   world  this is  new linw 
+       
+# #         ewjrnrwer
+      
+# #       """)
+
+# # print("hello   world  \nthis is  my first it program  \n  i  hpe  you all like  it")
+
+
+
+
+
+# # # why we  not  use  single quation 
+
+# # # print('  it's  going    to rain today ')
+      
+# # print('  it\'s  going    to rain today ')
+
+
+# # # comments in python 
+
+
+# # # single line  comment
+# # # multiple  line  comments 
+
+# # # in this program i will add two number 
+
+# # """
+
+# # in   this  anew  invention 
+
+# # """
+
+
+
+
+
+# # # variables 
+
+
+# # # variable   are placeholder 
+
+
+# # a ="hello world"
+# # print(a)
+
+
+# # # 1. python is case-sensitive language 
+
+
+# # # 2.make sure to not  use spaces while creating a variable 
+
+# # # use _  or  
+# # # Even_number 
+ 
+# # #  and evenNumber =
+
+
+# # # 3.a variable  name should  not  be start  with number   and  with special symbol 
+# # # 1Num 
+
+
+# # # @Hello = 45
+
+
+# # # user   input  and data type 
+
+# # # text type :string numberic type ->int,float,complex
+# # # sequence type:list,tuple,range 
+# # # mapping type :dict
+# # # set type : set, frozenset 
+# # # boolean type :bool 
+# # # binary type :bytes,bytearray ,memoryview 
+
+
+
+
+
+# # # user input 
+# # # name=input("enter your name here ")
+# # # print(name)
+
+
+
+
+
+
+# # name = input("enter your name")
+# # print(name)
+
+
+# # age = int(input("enter your age"))
+
+# # print(age)
+
+
+# # age1 = float(input("enter no"))
+# # print(age1)
+
+
+# # # integer  ke andar float not  used  it give an error  but 
+# # # float mein integre can be used 
+
+
+
+# # exp1  = eval(input("enetr any equation"))
+# # print(exp1)
+
+# # # 4+5 =>9
+
+
+# # # print("hello  world")
+# # # o/t->hello world
+
+
+
+
+# # # typescasting  abd  sub type 
+
+
+
+# # # # conversion of  one data type  into  another dataype 
+# # # implicit  type conversion -> where  python itself  converts  data type  to another /
+# # # explicit   type  conversion ->   where  user  convert  one datatype  to another data type 
+
+
+
+# # name="jhon"
+# # print(type(name))
+# # age =34
+# # print(type(age))
+
+
+# # # implicit 
+# # a =234
+# # b=1.34
+# # # c =a+b
+# # # print(c, type(c))  
+# # # // float 
+
+
+# # # explicit type  casting
+
+# # # a = int(a)
+# # a =float(a)
+# # print(type(a))
+# # c =a +b
+# #  ===========
+# # problem solving 
+
+
+# name  = "Eweq"
+# age =12
+# address = "ee2312 street"
+
+# # to swap two  avraible 
+# # using twmporary varibele 
+
+
+# # 1. x =22
+# # y =32
+# # temp =x
+# # x =y
+# # y =temp
+
+
+
+# x =12
+# y= 432
+
+
+
+# temp =x
+# x =y
+
+# print(temp)
+# y =temp
+# print(x)
+# print(y)
+
+
+# # 2.
+# a = 13
+# b =45
+# # left,right = right, left 
+# a,b = b,a
+
+
+# print(a)
+# print(b)
+
+
+
+# x = 12.4 
+# print(type(x),x)
+
+# x = int(x)
+# print(x)
+
+
+# name=input("enter name")
+# class1 = input("enter the grade ")
+# age = int(input("enter the age" ))
+
+# email = input("enter email ")
+# ph_no =  input(input("enter  phone number"))
+
+
+
+# print(name)
+
+
+# # operator  and operands 
+# """
+
+
+# x+  y  -> x and y are operands 
+# + -> operator 
+
+# arithmetic  operator 
+
+# comparision   operator 
+# logical operator 
+# assignment operator 
+# identity operator 
+# membership operator 
+#  bitwise operator 
+ 
+ 
+ 
+ 
+ 
+ 
+#  ariyhmetic   operator -> 
+#  +
+#  -
+#  *
+#  floor division (//)
+#  exponention (**)
+# modulus ->    8%3 -> 2   (remander )
+
+
+
+
+
+
+
+
+
+# """
+
+
+# print(8%3)
+
+# # fllor  division ->
+# # print(8//2)  -> 2.6 
+
+# print(10**3)
+
+# # comparision operator 
+
+# # less than  <
+# # less than equal to <=
+# # not  equal  to  !=
+
+
+# # equal to  ==
+# # greater  then or equal to  >= 
+
+# # greater than >
+
+
+print(3>2)
+# True
+
+print(3==6)
+
+print(3!= 10)
+
+
+print(3>=6)
